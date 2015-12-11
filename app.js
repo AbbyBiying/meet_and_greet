a3e72b01a9192a49cc20ec220a5a6fdaf0ad4169
AbbyBiying <abbyzhangnyc@gmail.com>
2015-12-10 20:51:14 -0500

    fix

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var app = angular.module('AbbyIntroduction', ['ui.router','ngAnimate',
   'AbbyIntroduction.utils.service', 'AbbyIntroduction.portfolios.service','AbbyIntroduction.portfolios'
   ])
-
-app.run(['$rootScope', '$state', '$stateParams', function ($rootScope,$state,$stateParams) {
+.run(['$rootScope', '$state', '$stateParams', function ($rootScope,$state,$stateParams) {
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
 }])
