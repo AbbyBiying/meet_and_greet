c624028be8b6160997d91fd67eb2447b93b05c20
AbbyBiying <abbyzhangnyc@gmail.com>
2015-12-16 21:52:13 -0500

    add material design

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-var app = angular.module('AbbyIntroduction', ['ui.router','ngAnimate',
+var app = angular.module('AbbyIntroduction', ['ui.router','ngAnimate', 'ngMaterial', 'ngMessages',
   'AbbyIntroduction.utils.service', 'AbbyIntroduction.portfolios.service','AbbyIntroduction.portfolios'
   ])
 .run(['$rootScope', '$state', '$stateParams', function ($rootScope,$state,$stateParams) {
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
 }])
+
+app.controller('AppCtrl', function($scope) {
+  $scope.title1 = 'Button';
+  $scope.title4 = 'Warn';
+  $scope.isDisabled = true;
+
+  $scope.googleUrl = 'http://google.com';
+
+});
+
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('purple')
+    .accentPalette('orange');
+});
